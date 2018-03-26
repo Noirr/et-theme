c6aac67ca9ac0f6e3cd7432b7705b3190bc605d2
Radek <radek@noirweb.pl>
2018-03-26 19:57:34 +0200

    1
    1

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+<div class="space-header"></div>
+
+
+<section class="section" id="section-o-nas">
+	<div class="container clearfix">
+		<h1>Strona nie została znaleziona</h1>
+		<h2 style="font-size:135px;">Błąd 404.</h2>
+		<p>Przepraszamy ale strona, której szukasz nie została znaleziona :(</p>
+		<p>Skorzystaj z menu, żeby znaleźć stronę, która Ciebie interesuje.</p>
+	</div>
+</section>
+
+
+
+
+
+<?php get_footer(); ?>
